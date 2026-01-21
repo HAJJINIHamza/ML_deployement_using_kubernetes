0277b1cadf1d69062bac7de86f5e69c107683490
HAJJINIHamza <hajjinihamza2@gmail.com>
2026-01-21 21:40:47 +0100

    Fixed : model tests shouldnt be ran in CI

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,16 @@
 import pickle
 
-# Import model
-with open("models/vanilla_model.pkl", "rb") as f_model:
-    model = pickle.load(f_model)
+import pytest
 
 
+@pytest.mark.model  # Mark it a model test
 def test_model():
+    # Import model
+    try:
+        with open("models/vanilla_model.pkl", "rb") as f_model:
+            model = pickle.load(f_model)
+    except FileNotFoundError:
+        raise FileNotFoundError("Model file not found")
     # test1
     data_1 = [[5.1, 3.5, 1.4, 0.2]]
     assert model.predict(data_1) == [0]
